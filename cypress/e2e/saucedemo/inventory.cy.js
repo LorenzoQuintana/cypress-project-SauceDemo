dd981b132a9c736f4dbfc4abb6e74f2557457b62
lorenzoQuintana <lorenzoquintana94@gmail.com>
2024-06-05 17:32:13 +0200

    Modification of credentials method

--- cypress/e2e/saucedemo/inventory.cy.js
+++ cypress/e2e/saucedemo/inventory.cy.js
@@ -1,45 +1,46 @@
+const Credentials = require('../../credentials');
+const LoginPage = require('../../pages/loginPage');
+
 describe("Inventory (Test Suite)", () => {
-  beforeEach(() => {
-    // Visit the page https://www.saucedemo.com/
-    cy.visit('/');
-    // Enter valid credentials in the username and password elements
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
-  });
+    beforeEach(() => {
+        // Visit the page https://www.saucedemo.com/
+        cy.visit('/');
+        // Use LoginPage class to login with valid credentials
+        LoginPage.login(Credentials.validUser.username, Credentials.validUser.password);
+    });
 
-  //-------------------------------------------------------------------------------------------------------
-  it('Validate the number of results', () => {
-    // Validate that the number of products displayed is equal to 6
-    cy.get('.inventory_item').should('have.length', 6);
-  });
-  
-  //-------------------------------------------------------------------------------------------------------
-  it('Increase cart value', () => {
-    // Add the Sauce Labs Bolt T-Shirt product to the cart
-    cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click();
-    // Validate that the cart icon has incremented to 1
-    cy.get('.shopping_cart_badge').should('contain', '1');
-  });
+    //-------------------------------------------------------------------------------------------------------
+    it('Validate the number of results', () => {
+        // Validate that the number of products displayed is equal to 6
+        cy.get('.inventory_item').should('have.length', 6);
+    });
 
-  //-------------------------------------------------------------------------------------------------------
-  it('Visibility of the Remove product from cart button', () => {
-    // Add the Sauce Labs Onesie product to the cart
-    cy.get('#add-to-cart-sauce-labs-onesie').click();
-    // Validate that, upon adding the product, the REMOVE button is visible
-    cy.get('.shopping_cart_badge').click();
-    cy.get('#remove-sauce-labs-onesie').should('be.visible');
-  });
+    //-------------------------------------------------------------------------------------------------------
+    it('Increase cart value', () => {
+        // Add the Sauce Labs Bolt T-Shirt product to the cart
+        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click();
+        // Validate that the cart icon has incremented to 1
+        cy.get('.shopping_cart_badge').should('contain', '1');
+    });
 
-  //-------------------------------------------------------------------------------------------------------
-  it('Remove product from cart', () => {
-    // Add the Sauce Labs Onesie product to the cart
-    cy.get('#add-to-cart-sauce-labs-onesie').click();
-    // Remove the product from the cart
-    cy.get('.shopping_cart_badge').click();
-    cy.get('#remove-sauce-labs-onesie').click();
-    // Validate that the product has been removed from the cart icon
-    cy.get('.shopping_cart_badge').should('not.exist');
-  });
+    //-------------------------------------------------------------------------------------------------------
+    it('Visibility of the Remove product from cart button', () => {
+        // Add the Sauce Labs Onesie product to the cart
+        cy.get('#add-to-cart-sauce-labs-onesie').click();
+        // Validate that, upon adding the product, the REMOVE button is visible
+        cy.get('.shopping_cart_badge').click();
+        cy.get('#remove-sauce-labs-onesie').should('be.visible');
+    });
+
+    //-------------------------------------------------------------------------------------------------------
+    it('Remove product from cart', () => {
+        // Add the Sauce Labs Onesie product to the cart
+        cy.get('#add-to-cart-sauce-labs-onesie').click();
+        // Remove the product from the cart
+        cy.get('.shopping_cart_badge').click();
+        cy.get('#remove-sauce-labs-onesie').click();
+        // Validate that the product has been removed from the cart icon
+        cy.get('.shopping_cart_badge').should('not.exist');
+    });
 });
 
--- cypress/e2e/saucedemo/login.cy.js
+++ cypress/e2e/saucedemo/login.cy.js
@@ -1,39 +1,38 @@
+const Credentials = require('../../credentials');
+const LoginPage = require('../../pages/loginPage');
+
 describe("Login (Test Suite)", () => {
-  beforeEach(() => {
-    // Visit the page https://www.saucedemo.com/
-    cy.visit('/');
-  });
+    beforeEach(() => {
+        // Visit the page https://www.saucedemo.com/
+        cy.visit('/');
+    });
 
-  //-------------------------------------------------------------------------------------------------------
-  it('Validate landing URL and page title is as expected', () => {
-    // Validate that the URL is the same as the one we requested to navigate to
-    cy.url().should('eq', Cypress.config().baseUrl);
+    //-------------------------------------------------------------------------------------------------------
+    it('Validate landing URL and page title is as expected', () => {
+        // Validate that the URL is the same as the one we requested to navigate to
+        cy.url().should('eq', Cypress.config().baseUrl);
 
-    // Validate that the page title is “Swag Labs”
-    cy.title().should('eq', 'Swag Labs');
-  });
-  
-  //-------------------------------------------------------------------------------------------------------
-  it('Validate a user can log into the page with valid credentials', () => {
-    // Enter valid credentials in the username and password elements
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
+        // Validate that the page title is “Swag Labs”
+        cy.title().should('eq', 'Swag Labs');
+    });
 
-    // Validate that the user logs in successfully (URL and Title)
-    cy.url().should('include', '/inventory.html');
-    cy.title().should('eq', 'Swag Labs');
-  });
+    //-------------------------------------------------------------------------------------------------------
+    it('Validate a user can log into the page with valid credentials', () => {
+        // Use LoginPage class to login with valid credentials
+        LoginPage.login(Credentials.validUser.username, Credentials.validUser.password);
 
-  //--------------------------------------------------------------------------------------------------------
-  it('Validate a user cannot log into the page with invalid credentials', () => {
-    // Enter invalid credentials in the username and password elements
-    cy.get('#user-name').type('standard_use');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
+        // Validate that the user logs in successfully (URL and Title)
+        cy.url().should('include', '/inventory.html');
+        cy.title().should('eq', 'Swag Labs');
+    });
 
-    // Validate that an error message is displayed with the expected message
-    cy.get('.error-message-container').should('be.visible')
-      .and('contain', 'Epic sadface: Username and password do not match any user in this service');
-  });
+    //--------------------------------------------------------------------------------------------------------
+    it('Validate a user cannot log into the page with invalid credentials', () => {
+        // Use LoginPage class to login with invalid credentials
+        LoginPage.login(Credentials.invalidUser.username, Credentials.invalidUser.password);
+
+        // Validate that an error message is displayed with the expected message
+        cy.get('.error-message-container').should('be.visible')
+            .and('contain', 'Epic sadface: Username and password do not match any user in this service');
+    });
 });
